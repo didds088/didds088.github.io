89cbea08e1f484a3735b0e99f707f00498f2f0eb
Agusiobo Kingsley <agusiobokingsley@outlook.com>
2022-11-21 22:40:18 +0100

    first deployment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import {Link} from "react-router-dom";
+import $ from "jquery";
 
-function App() {
+//load database 
+$.ajax({
+	type: "POST",
+	url: "https://teakings.000webhostapp.com/viewTAB.php",                  
+    data: {},
+	dataType: 'json',
+	crossDomain: true,
+	beforeSend: function() {
+                      //preloader
+	},
+	success: function(response) {  
+	$.each(response, function(i, field) { 	
+	let name = field.name;
+	let type = field.type;
+	let unique = field.sku;
+	let price = field.price;
+	let measure = field.measurement;
+	let weight = field.value;
+	let id = field.id;
+	if(response!=="0") 
+	{       
+		const create = document.createElement("div");
+		create.innerHTML = '<section class="tab"><div class="title"><input class="form-check-input delete-checkbox" type="checkbox" name="option1" value="'+id+'" /></div><div class="product-items"><p id="'+id+'">'+unique+'</p><p>'+name+'</p><p class="text-primary">'+price+'<span> $</span></p><p>'+measure+': <span>'+weight+'</span></p></div></section>';
+		document.getElementById("lists").appendChild(create);
+	}
+	else{
+		alert("error");
+	}
+   }); //each. Response
+  }// success function
+ }); //ajax ends
+ 
+ // mass delete button 
+const remove = () => {
+	let selected = [];
+	let checkboxes = document.querySelectorAll('input[type=checkbox]:checked')
+
+	for (let i = 0; i < checkboxes.length; i++) {
+  		selected = Array.from(checkboxes).map(x => x.value);
+	}
+
+let deleting = "";
+selected.forEach(deleted);
+
+//pass checkbox arrays into ajax data
+function deleted(item) {
+deleting += $.ajax({
+	type: "POST",
+	url: "https://teakings.000webhostapp.com/delete.php",
+	data: {
+		selected : item
+	},
+	crossDomain: true,
+	cache: false,
+	beforeSend: function() {
+	},
+	success: function(data) {
+	if (data != "error") {                   
+		window.location.reload();
+		}
+	}
+}); //ajax ends     
+  
+}//deleted function ends
+}//remove ends
+ 
+ 
+const App =()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+      <div className="rows pt-4">
+      <div className="float-start">
+	  <h1 id="heading">Product List</h1></div>
+	  <div className="float-end">
+	  <Link className="link btn btn-danger me-3" to="/Add">Add</Link>
+	  <button type="button" class="btn btn-secondary delete-checkbox" onClick={remove}>Mass Delete</button>
     </div>
+    
+    </div>    
+	 <div className="clearfix"></div>
+	<hr className="border border-danger"/>
+      <div id="lists" className="rows">
+      
+    <div className="clearfix"></div>
+      </div>
+        
+        <div className="footer">  
+        
+        <div className="clearfix"></div>
+        <hr className="border border-danger mt-5"/>
+      <h5 className="text-center">Scandiweb Test Assignment</h5>
+	  </div>
+      </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import App from './App';
+import Add from './Add';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+          <Route index element={<App />} />
+          <Route path="/Add" element={<Add />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
