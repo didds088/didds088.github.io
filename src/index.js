b1c0d63c3ef6dd2078663065eb8afcd4b7496dd8
Agusiobo <Agusiobokingsley@outlook.com>
2022-12-16 08:26:29 +0100

    source code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { BrowserRouter, Routes, Route} from "react-router-dom";
-import App from './App';
-import Add from './Add';
+import App from './ProductList';
+import Add from './AddList';
  
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -26,7 +25,3 @@ root.render(
   
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
